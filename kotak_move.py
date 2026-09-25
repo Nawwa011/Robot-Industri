da49cafd84a8662feabaa8aef6d451cb62008bd4
Muhammad Nawwarudin <awarudin987@gmail.com>
2026-09-25 13:36:33 +0700

    Add right rotation logic for elapsed time condition

--- kotak_move.py
+++ kotak_move.py
@@ -45,6 +45,10 @@ class MoverNode(Node):
             msg.linear.x = 0.5 # Kecepatan linear (m/s)
             msg.angular.z = 0.0  
             self.get_logger().info('Gerak Maju 3s')
+        elif elapsed_time < 24.0:
+            msg.linear.x = 0.0
+            msg.angular.z = -0.5236  # Kecepatan rotasi (rad/s)
+            self.get_logger().info('Rotasi Kanan 90 Derajat')
         else:
             msg.linear.x = 0.0
             msg.angular.z = 0.0
